0957361608f4191c2e5849d71672676b260f6401
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-03-13 23:04:35 +0700

    Fix-Bug

--- RecommendationWebsite/tailwind.config.cjs
+++ RecommendationWebsite/tailwind.config.cjs
@@ -1,31 +1,31 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   corePlugins: {
-    container: false
+    container: false,
   },
   theme: {
     extend: {
       colors: {
-        orange: '#ee4d2d'
+        orange: '#ee4d2d',
       },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' }
+          '50%': { transform: 'rotate(3deg)' },
         },
         special: {
           '0%': { width: '10%' },
-          '100%': { width: '100%' }
-        }
+          '100%': { width: '100%' },
+        },
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite'
-      }
-    }
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [
     plugin(function ({ addComponents, theme }) {
@@ -35,18 +35,18 @@ module.exports = {
           marginLeft: 'auto',
           marginRight: 'auto',
           paddingLeft: theme('spacing.4'),
-          paddingRight: theme('spacing.4')
+          paddingRight: theme('spacing.4'),
         },
         '.customLine': {
           content: '',
           bottom: '-5px',
           left: '0',
           width: '100%',
-          color: 'rgb(240 82 82 / var(--tw-text-opacity))'
-        }
-      })
+          color: 'rgb(240 82 82 / var(--tw-text-opacity))',
+        },
+      });
     }),
-    require('flowbite/plugin'),
-    require('@tailwindcss/line-clamp')
-  ]
-}
+
+    require('@tailwindcss/line-clamp'),
+  ],
+};
