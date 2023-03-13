7343b6b583c8c0298a2bf118aeefd1956e291771
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-03-13 22:52:06 +0700

    fdsafdsa

--- RecommendationWebsite/src/main.tsx
+++ RecommendationWebsite/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import { Flowbite } from 'flowbite-react'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
